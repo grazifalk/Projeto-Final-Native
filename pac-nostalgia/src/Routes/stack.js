a7ebd4288b92a69f2fa421c65f206e1444bfd86b
Márcia Almeida <97288400+Marcia-Almeida@users.noreply.github.com>
2022-11-20 13:19:44 -0300

    tela cadastro

--- pac-nostalgia/src/Routes/stack.js
+++ pac-nostalgia/src/Routes/stack.js
@@ -3,6 +3,7 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import Login from "../screens/Login";
 import RememberPassword from "../screens/RememberPassword/index.js";
 import ChangePassword from "../screens/ChangePassword/index.js";
+import Registration from "../screens/Registration/index.js";
 
 const { Screen, Navigator } = createNativeStackNavigator();
 
@@ -16,6 +17,7 @@ export function StackRoutes() {
       <Screen name="Login" component={Login} />
       <Screen name="RememberPassword" component={RememberPassword} />
       <Screen name="ChangePassword" component={ChangePassword} />
+      <Screen name="Registration" component={Registration}/>
     </Navigator>
   );
 }
