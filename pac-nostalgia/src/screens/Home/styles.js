3fc11b060926f8435515a263033cf72209d90a5e
Grazi Falk <83370784+grazifalk@users.noreply.github.com>
2022-11-20 22:29:07 -0300

    Menu inferior + colors theme

---
+++ pac-nostalgia/src/screens/Home/styles.js
@@ -0,0 +1,21 @@
+import styled from "styled-components/native";
+
+export const Container = styled.View`
+  flex: 1;
+  background-color: #689999;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const Logo = styled.Image`
+  width: 232px;
+  height: 170px;
+  margin-bottom: 50px;
+`;
+
+export const Recover = styled.Text`
+  font-weight: 400;
+  font-size: 18px;
+  color: #ffffff;
+  margin-bottom: 40px
+`;
