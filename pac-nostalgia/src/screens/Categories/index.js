c9e5ed08371899c5445ee49e11ced70d5b627cc9
Priscila Durão <pd.b@yahoo.com>
2022-11-25 10:57:11 -0300

    Up tela Categoria

--- pac-nostalgia/src/screens/Categories/index.js
+++ pac-nostalgia/src/screens/Categories/index.js
@@ -1,12 +1,18 @@
-import { MainContainer } from "./styles.js";
+import { MainContainer, Up, Title } from "./styles.js";
 import { Api } from "../../Services";
-import { FlatList } from "react-native";
+import { FlatList, View } from "react-native";
 import { ItemCategory } from "../../components/ItemCategory/index.js";
 import { useState, useEffect } from "react";
 import { Menu } from "../../components/Menu";
+import { Feather } from "@expo/vector-icons";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+import { TouchableOpacity } from "react-native";
+import colors from "../../themes/colors";
+import { useNavigation } from "@react-navigation/native";
 
 export const Categories = () => {
   const [category, setCategory] = useState([]);
+  const navigation = useNavigation();
 
   const renderItem = ({ item }) => (
     <ItemCategory name={item.nome} photo={item.foto} />
@@ -22,8 +28,23 @@ export const Categories = () => {
   };
 
   return (
-    <MainContainer>      
-      <FlatList style={{marginTop: "100px"}}
+    <MainContainer>
+      <Up>
+        <TouchableOpacity onPress={() => navigation.navigate("Home")}>
+          {/* /*fazer rota search*\ */}
+          <Feather name="search" size={35} color={colors.tertiary} />
+        </TouchableOpacity>
+        <Title>CATEGORIA</Title>
+        <TouchableOpacity onPress={() => navigation.navigate("Cart")}>
+          <MaterialCommunityIcons
+            name="cart-outline"
+            size={35}
+            color={colors.tertiary}
+          />
+        </TouchableOpacity>
+      </Up>
+      <FlatList
+        style={{ marginTop: "100px" }}
         data={category}
         keyExtractor={(item) => item.id}
         renderItem={renderItem}
