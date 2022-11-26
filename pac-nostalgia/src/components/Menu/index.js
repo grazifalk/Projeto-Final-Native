51bbe33970de0ef27b65b093f2b7365993f0d1c3
Priscila Durão <pd.b@yahoo.com>
2022-11-25 21:29:53 -0300

    Atualização projeto

--- pac-nostalgia/src/components/Menu/index.js
+++ pac-nostalgia/src/components/Menu/index.js
@@ -2,23 +2,19 @@ import colors from "../../themes/colors.js";
 import { TouchableOpacity } from "react-native";
 import { Ionicons } from "@expo/vector-icons";
 import { FontAwesome } from "@expo/vector-icons";
-import { MaterialIcons } from "@expo/vector-icons";
 import { Container } from "./styles.js";
 import { useNavigation } from "@react-navigation/native";
 
 export const Menu = () => {
-    const navigation = useNavigation();
+  const navigation = useNavigation();
 
   return (
     <Container>
       <TouchableOpacity onPress={() => navigation.navigate("Home")}>
         <Ionicons name={"home"} size={35} color={colors.tertiary} />
       </TouchableOpacity>
-        <TouchableOpacity onPress={() => navigation.navigate("Categories")}>  
+      <TouchableOpacity onPress={() => navigation.navigate("Categories")}>
         <Ionicons name={"list"} size={35} color={colors.tertiary} />
-      </TouchableOpacity>
-      <TouchableOpacity onPress={() => navigation.navigate("Cart")}>
-        <MaterialIcons name={"favorite"} size={35} color={colors.tertiary} />
       </TouchableOpacity>
       <TouchableOpacity onPress={() => navigation.navigate("Login")}>
         <FontAwesome name={"user-circle-o"} size={35} color={colors.tertiary} />
